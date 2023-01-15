cf3fdf7d5ecb8dc839149b979fd97ded3a88c0e1
DimitarZlatev10 <aguerofc@abv.bg>
2023-01-15 08:55:46 +0200

    created and tested login

--- Angular Application/hardware-site-project/src/app/auth/login/login.component.ts
+++ Angular Application/hardware-site-project/src/app/auth/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { LocalService } from 'src/app/local.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private localStorage: LocalService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loginUser() {
+    console.log(this.email, this.password);
+    this.api
+      .login({
+        email: this.email,
+        password: this.password,
+      })
+      .subscribe({
+        next: (value) => {
+          this.localStorage.saveData('token', this.email);
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
   }
 
+  ngOnInit(): void {}
 }
