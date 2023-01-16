84ddb2ba84e7567ad5e148ee16cb72baefbd4d5a
DimitarZlatev10 <aguerofc@abv.bg>
2023-01-16 23:16:17 +0200

    made some changes on details component

--- Angular Application/hardware-site-project/src/app/products/details/details.component.ts
+++ Angular Application/hardware-site-project/src/app/products/details/details.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { LocalService } from 'src/app/local.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
+  id: string | null = '';
+  item: any = [];
+  isLogged: string | null = null;
+  userInfo: any = [];
+  errorMessage = '';
 
-  constructor() { }
+  updatePage() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.isLogged = this.localService.getData('token');
 
-  ngOnInit(): void {
+    this.api.getProductById(this.id).subscribe({
+      next: (value) => {
+        this.item = value;
+        console.log(value);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+
+    const email = this.localService.getData('token');
+    this.api.getUserData(email).subscribe({
+      next: (value) => {
+        this.userInfo = value;
+        console.log(value);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
 
+  constructor(
+    private route: ActivatedRoute,
+    private localService: LocalService,
+    private api: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.updatePage();
+  }
 }
